26fc1e7350f8c9f0093312d7bffa39efe9753c64
ret7020 <ret7020@gmail.com>
2023-03-18 11:38:23 +0300

    Work with auth + jwt + web socks

--- server.py
+++ server.py
@@ -6,8 +6,6 @@ from fastapi.responses import HTMLResponse
 import json
 import core.config as conf
 import auth as auth_endpoint
-from database import init_models, get_session
-import asyncio
 from fastapi_jwt_auth import AuthJWT
 
 
@@ -22,8 +20,9 @@ html = """
         <title>Chat</title>
     </head>
     <body>
-        <h1>WebSocket Chat</h1>
+        <h1>WebSocket</h1>
         <h2>Your ID: <span id="ws-id"></span></h2>
+        <input type="text" id="jwt" autocomplete="off" placeholder="JWT Token" value="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJyZXQ3MDIwQGdtYWlsLmNvbSIsImlhdCI6MTY3OTEyNjA4MCwibmJmIjoxNjc5MTI2MDgwLCJqdGkiOiIzMWEzZTEzZS1jMjQyLTRiMWUtOWMwZS0yZjllOWRmYTVmNjYiLCJleHAiOjE2NzkxMjY5ODAsInR5cGUiOiJhY2Nlc3MiLCJmcmVzaCI6dHJ1ZX0.2DcIFnbySsA3aNSxEam9S-84oQd_tOooO1fn5MHuXOU"/>
         <form action="" onsubmit="sendMessage(event)">
             <input type="text" id="messageText" autocomplete="off"/>
             <button>Send</button>
@@ -32,8 +31,9 @@ html = """
         </ul>
         <script>
             var client_id = Date.now();
-            var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJyZXQ3MDIwQGdtYWlsLmNvbSIsImlhdCI6MTY3OTA4ODYxNiwibmJmIjoxNjc5MDg4NjE2LCJqdGkiOiJmZGQ4MWY0Ny05NjI3LTQxMDUtYmUwMS05MzFhY2MyNjNlZTEiLCJleHAiOjE2NzkwODk1MTYsInR5cGUiOiJhY2Nlc3MiLCJmcmVzaCI6dHJ1ZX0.bMyTYXFaGftuis72ZNznw0A4_aJiUEdrkiam6_9OcJM";
+            var token = document.getElementById("jwt").value;
             document.querySelector("#ws-id").textContent = client_id;
+            
             var ws = new WebSocket(`ws://localhost:8080/ws/${client_id}?token=${token}`);
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
@@ -44,7 +44,7 @@ html = """
             };
             function sendMessage(event) {
                 var input = document.getElementById("messageText")
-                ws.send(input.value)
+                ws.send(JSON.stringify({"data": input.value}))
                 input.value = ''
                 event.preventDefault()
             }
@@ -52,6 +52,7 @@ html = """
     </body>
 </html>
 """
+
 
 class WebSocketParser:
     def __init__(self):
@@ -87,25 +88,23 @@ async def get():
 
 @app.websocket("/ws/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: int, token: str = Query(...), Authorize: AuthJWT = Depends()):
-    print(token)
     Authorize.jwt_required("websocket", token=token)
+    decoded_token = Authorize.get_raw_jwt(token)
     await manager.connect(websocket)
     try:
         while True:
             try:
                 data = await websocket.receive_json()
                 print(data)
-                for i in range(10):
-                    await manager.send_personal_message(f"Answer from server", websocket)
+                await manager.send_personal_message(f"Echo: {data['data']}", websocket)
             except json.decoder.JSONDecodeError:
-               await manager.send_personal_message(f"Unprocessable message", websocket)
-            #await manager.broadcast(f"Client #{client_id} says: {data}")
+                await manager.send_personal_message(f"Unprocessable message", websocket)
+            # await manager.broadcast(f"Client #{client_id} says: {data}")
     except WebSocketDisconnect:
         manager.disconnect(websocket)
         print(f"Client disconnected")
 
-        #await manager.broadcast(f"Client #{client_id} left the chat")
-
 if __name__ == "__main__":
     # asyncio.run(init_models())
-    uvicorn.run("server:app", port=conf.HTTP_PORT, host=conf.HTTP_HOST, reload=True, headers=[("server", "By @ret7020")])
+    uvicorn.run("server:app", port=conf.HTTP_PORT, host=conf.HTTP_HOST,
+                reload=True, headers=[("server", "By @ret7020")])
